1f1d667985cf12ec290464d1cd9c57239b090bd9
filipkk78 <filipkk78@gmail.com>
2024-11-27 20:52:50 +0100

    Add next hours carousel

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ let weather = {};
 
 async function getWeather() {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${weatherQuery}/today?unitGroup=metric&key=HE6349R9L33N4J5GUV6UAVHH5&contentType=json`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${weatherQuery}/next24hours?unitGroup=metric&key=HE6349R9L33N4J5GUV6UAVHH5&contentType=json`,
     {
       mode: "cors",
     }
   );
   const responseJson = await response.json();
   weather = responseJson;
-  console.log(weather);
   loadCard();
 }
 
@@ -45,10 +44,15 @@ async function success(pos) {
       mode: "cors",
     }
   );
-  console.log(`${crd.latitude}, ${crd.longitude}`);
   const responseJson = await response.json();
-  const cityName = responseJson.address.city;
-  console.log(cityName);
+  let cityName;
+  if (responseJson.address.city) {
+    cityName = responseJson.address.city;
+  } else if (responseJson.address.town) {
+    cityName = responseJson.address.town;
+  } else {
+    cityName = responseJson.address.village;
+  }
   weatherQuery = cityName;
   getWeather();
 }
@@ -82,6 +86,99 @@ function loadCard() {
 
   const pressure = document.querySelector(".athm-pressure");
   pressure.textContent = `${weather.currentConditions.pressure} mm Hg`;
+
+  let hours;
+  if (!weather.days[1]) {
+    hours = weather.days[0].hours;
+  } else {
+    const time = new Date().getHours();
+    let currentDay = weather.days[0].hours.slice(time);
+    let nextDay = weather.days[1].hours.slice(0, time);
+    hours = currentDay.concat(nextDay);
+  }
+
+  hours.forEach((hour) => {
+    const hourTab = document.querySelector(`#nextHour${hours.indexOf(hour)}`);
+
+    const hourTitle = document.querySelector(
+      `#nextHour${hours.indexOf(hour)} p`
+    );
+
+    hourTitle.textContent = hour.datetime.substring(0, 5);
+
+    const hourIcon = document.querySelector(
+      `#nextHour${hours.indexOf(hour)} div`
+    );
+
+    hourIcon.className = `hour-icon ${hour.icon}`;
+
+    const hourTemp = document.querySelector(
+      `#nextHour${hours.indexOf(hour)} h3`
+    );
+
+    hourTemp.textContent = `${Math.round(hour.temp)}°C`;
+
+    hourTitle.className = "hour";
+
+    if (hours.indexOf(hour) > 7) {
+      hourTab.style.display = "none";
+    }
+  });
+
+  // console.log(hours);
+
+  let carouselPos = 0;
+
+  const hourTabs = document.querySelectorAll(".next-hour");
+
+  function loadHours(hour) {
+    hourTabs.forEach((hourTab) => {
+      hourTab.style.display = "none";
+    });
+    let i = hour + 8;
+    for (hour; hour < i; hour++) {
+      hourTabs[hour].style.display = "flex";
+    }
+  }
+
+  function carouselPrev() {
+    arrowRight.style.display = "block";
+    arrowPlaceholderRight.style.display = "none";
+    carouselPos--;
+    console.log(carouselPos);
+    if (carouselPos === 0) {
+      arrowLeft.style.display = "none";
+      arrowPlaceholderLeft.style.display = "block";
+    }
+    loadHours(carouselPos);
+  }
+
+  function carouselNext() {
+    arrowLeft.style.display = "block";
+    arrowPlaceholderLeft.style.display = "none";
+    carouselPos++;
+    console.log(carouselPos);
+    if (carouselPos === 16) {
+      arrowRight.style.display = "none";
+      arrowPlaceholderRight.style.display = "block";
+    }
+    loadHours(carouselPos);
+  }
+
+  const arrowLeft = document.querySelector("#carousel-arrow-left");
+  arrowLeft.addEventListener("click", carouselPrev);
+  arrowLeft.style.display = "none";
+  const arrowPlaceholderLeft = document.querySelector(
+    "#carousel-arrow-placeholder-left"
+  );
+  arrowPlaceholderLeft.style.display = "block";
+
+  const arrowRight = document.querySelector("#carousel-arrow-right");
+  arrowRight.addEventListener("click", carouselNext);
+  const arrowPlaceholderRight = document.querySelector(
+    "#carousel-arrow-placeholder-right"
+  );
+  arrowPlaceholderRight.style.display = "none";
 }
 
 document.querySelector("#title").addEventListener("click", loadCard);
--- src/modules/card.js
+++
@@ -1,8 +0,0 @@
-import weather from "..";
-
-function loadCard() {
-  const cityName = document.querySelector(".city-name");
-  cityName.textContent = weather.address;
-}
-
-export default loadCard;
